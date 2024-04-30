dcb59049c08a4a10d8d11e4b9967f5822433b115
Tin Thai <tthaithanh@investcloud.com>
2024-05-01 04:36:22 +0700

    [CONF] Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {RouterProvider, } from "react-router";
+import appRouter from "./app.router";
+import {AuthenticatedProvider} from "./shared/Authenticated";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthenticatedProvider>
+            <RouterProvider router={appRouter} />
+
+        </AuthenticatedProvider>
+    )
 }
 
 export default App;
